3f76289f1e671419db2536aa593f02ef5422c8f5
Michael Gudzevskyi <hello@michaelgudzevskyi.com>
2021-03-09 20:53:06 -0300

    Remove caches version

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,7 +1,29 @@
-self.addEventListener('install', event => {
-    console.log('[SV]: install');
+const staticCacheName = 's-app-v1'
+const assetUrls = [
+    'index.html',
+    '/js/app.js',
+    '/css/style.css'
+]
+
+self.addEventListener('install', async event => {
+    const cache = await caches.open(staticCacheName)
+    await cache.addAll(assetUrls)
 })
 
-self.addEventListener('activate', event => {
-    console.log('[SV]: activate');
+self.addEventListener('activate', async event => {
+    const cacheNames = await caches.keys()
+    await Promise.all(
+        cacheNames
+            .filter(name => name !== staticCacheName)
+            .map(name => caches.delete(name))
+    )
 })
+
+self.addEventListener('fetch', event => {
+    event.respondWith(cacheFirst(event.request))
+})
+
+const cacheFirst = async (request) => {
+    const cached = await caches.match(request)
+    return cached ?? await fetch(request)
+} 
