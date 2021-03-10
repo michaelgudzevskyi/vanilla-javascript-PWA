841706d79389b61c59a6ed9ee3f0ae3671c6c757
Michael Gudzevskyi <hello@michaelgudzevskyi.com>
2021-03-09 21:13:17 -0300

    Offline

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,6 +1,8 @@
 const staticCacheName = 's-app-v1'
+const dynamicCacheName = 'd-app-v1'
 const assetUrls = [
     'index.html',
+    'offline.html',
     '/js/app.js',
     '/css/style.css'
 ]
@@ -15,15 +17,34 @@ self.addEventListener('activate', async event => {
     await Promise.all(
         cacheNames
             .filter(name => name !== staticCacheName)
+            .filter(name => name !== dynamicCacheName)
             .map(name => caches.delete(name))
     )
 })
 
 self.addEventListener('fetch', event => {
-    event.respondWith(cacheFirst(event.request))
+    const { request } = event
+    const url = new URL(request.url)
+    if (url.origin === location.origin) {
+        event.respondWith(cacheFirst(request))
+    } else {
+        event.respondWith(networkFirst(request))
+    }
 })
 
 const cacheFirst = async (request) => {
     const cached = await caches.match(request)
     return cached ?? await fetch(request)
+} 
+
+const networkFirst = async (request) => {
+    const cache = await caches.open(dynamicCacheName)
+    try {
+        const response = await fetch(request)
+        await cache.put(request, response.clone())
+        return response
+    } catch (error) {
+        const cached = await cache.match(request)
+        return cached ?? await caches.match('/offline.html')
+    }
 } 
